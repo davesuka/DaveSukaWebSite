488554b561805f41a613cf9a5907a7be94065c8d
David Grada <david.grada@hotmail.com>
2022-04-28 14:41:50 -0500

    Integrate code

---
+++ DaveSukaPlatziWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DaveSukaPlatziWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
